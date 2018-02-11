a9e7960ccc0d41ca6dcd388d98dbf7f3f60ea720
Dorota Jarecka <djarecka@gmail.com>
2018-02-10 21:06:13 -0500

    Revert "printing worflowname"
    This reverts commit 859a998fb185725990934b3f1a35cdddb27d137e.

--- testrunner.py
+++ testrunner.py
@@ -11,7 +11,6 @@ Workflows_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
 
 if __name__ == "__main__":
     for workflow in next(os.walk(Workflows_dir))[1]:
-        print("WORKFLOW NAME", workflow)
         wf = WorkflowRegtest(os.path.join(Workflows_dir, workflow))
         wf.run()
         wf.merging_output()

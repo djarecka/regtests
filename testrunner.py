859a998fb185725990934b3f1a35cdddb27d137e
Dorota Jarecka <djarecka@gmail.com>
2018-02-10 20:45:28 -0500

    printing worflowname

--- testrunner.py
+++ testrunner.py
@@ -11,6 +11,7 @@ Workflows_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
 
 if __name__ == "__main__":
     for workflow in next(os.walk(Workflows_dir))[1]:
+        print("WORKFLOW NAME", workflow)
         wf = WorkflowRegtest(os.path.join(Workflows_dir, workflow))
         wf.run()
         wf.merging_output()
